a20be2404218bcd1ab344f132fe52ae007d2c6d7
Anhlaidh <chinahantianzhao@foxmail.com>
2020-02-28 21:31:12 +0800

    git init repo

---
+++ src/main/java/com/namesapce/community/Controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.namesapce.community.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Description:
+ * @author: Anhlaidh
+ * @date: 2020/2/28 0028 21:15
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
