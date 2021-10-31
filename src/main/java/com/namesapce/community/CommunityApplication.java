a583b2b006f03664c5ba196ac78d6b81060d9e33
Anhlaidh <chinahantianzhao@foxmail.com>
2021-10-31 17:37:56 +0800

    .

--- src/main/java/com/namesapce/community/CommunityApplication.java
+++ src/main/java/com/namesapce/community/CommunityApplication.java
@@ -3,8 +3,9 @@ package com.namesapce.community;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @MapperScan(basePackages = "com.namesapce.community.mapper")
 public class CommunityApplication {
 
