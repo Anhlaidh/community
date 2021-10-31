1169beabfe1f2a86e29ea44ef919746545b9401e
Anhlaidh <chinahantianzhao@foxmail.com>
2021-10-31 17:43:33 +0800

    .

--- src/main/java/com/namesapce/community/CommunityApplication.java
+++ src/main/java/com/namesapce/community/CommunityApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@MapperScan(basePackages = "com.namesapce.community.mapper")
+@SpringBootApplication
+@MapperScan(basePackages = "com.namesapce.community.Mapper")
 public class CommunityApplication {
 
     public static void main(String[] args) {
